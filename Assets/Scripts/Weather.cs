244e62cf7ad750f3f418a84407ca3935a7252ce8
karabasium <ivanmyakishev@gmail.com>
2018-11-17 17:37:08 +0300

    Very beginning of weather implementation

---
+++ Assets/Scripts/Weather.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather : MonoBehaviour {
+	//private int[] l1 = new int[4] = {}; 
+	private int[][] directions = new int [][] { new int[] { -1, 1  }, new int[] { 0, 1 },   new int[] { 1, 1  },
+												new int[] { -1, 0  },                       new int[] { 1, 0  },
+												new int[] { -1, -1 }, new int[] { 0, -1 }, new int[]  { 1,-1 }};
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
